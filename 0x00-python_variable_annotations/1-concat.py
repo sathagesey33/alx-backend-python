9a8cae24e6ac7b4e99aac9f092fe08a9ca05292c
sathagesey33 <abdukdirmuhanad@gmail.com>
2024-03-14 23:57:43 +0300

    . Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for concatenating two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
+
+
+if __name__ == "__main__":
+    str1 = "egg"
+    str2 = "shell"
+
+    print(concat(str1, str2) == "{}{}".format(str1, str2))
+    print(concat.__annotations__)
