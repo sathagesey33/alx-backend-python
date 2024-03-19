d706ce2b813c6f8c2d716112714aff9c359248d3
sathagesey33 <abdukdirmuhanad@gmail.com>
2024-03-19 22:17:21 +0300

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""Creates a generator"""
+""" Creates a generator """
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Each time asynchronously waits 1 second,
-        then yield a random number between 0 and 10"""
+    """
+    Each time asynchronously waits 1 second,
+        then yield a random number between 0 and 10
+    """
     for _ in range(0, 10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform(0, 10)\
         
